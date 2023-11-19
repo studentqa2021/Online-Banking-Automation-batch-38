e6322e8dff8b6e811e1822f2a0ebdc6cd894c12a
studentqa2021 <sarowerhome@gmail.com>
2023-11-19 08:56:11 -0500

    first commit

---
+++ src/main/java/com/generic/Login.java
@@ -0,0 +1,17 @@
+package com.generic;
+
+public class Login { 
+	 static int salary = 9000;
+	 static String name = "Hosna";
+	 static double price = 2.75;
+ int  mysalary = 5000;
+	public static void main(String[] args) {
+		
+	 int a = 10;
+	 System.out.println("Hellow ");
+	 System.out.println(a);
+	 System.out.println(salary);
+	 System.out.println(name);
+	 System.out.println( price);
+}
+}
